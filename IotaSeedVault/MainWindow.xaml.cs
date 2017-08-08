0446c8e7c9f0f2942546160b26b7021ae3b1f783
jonas six <jonas.six452@gmail.com>
2017-08-08 21:08:32 +0200

    Add project files.

---
+++ IotaSeedVault/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using IotaSeedVault.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IotaSeedVault
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<IotaSeed> tempData = new ObservableCollection<IotaSeed>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenFakeData();
+            LoadFakeData();
+        }
+
+        public void GenFakeData()
+        {
+            tempData.Clear();
+            tempData.Add(IotaSeed.generateIotaSeed());
+            tempData.Add(IotaSeed.generateIotaSeed());
+            tempData.Add(IotaSeed.generateIotaSeed());
+        }
+        public void LoadFakeData()
+        {
+            dgData.DataContext = tempData;
+        }
+    }
+}
---
+++ IotaSeedVault/Model/IotaSeed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotaSeedVault.Model
+{
+    public class IotaSeed
+    {
+        public int Id { get; set; }
+        public string Seed { get; set; }
+        public string Name { get; set; }
+        public string Remark { get; set; }
+
+        public IotaSeed()
+        {
+
+        }
+
+        public IotaSeed(int id, string seed, string name, string remark)
+        {
+            Id = id;
+            Seed = seed;
+            Name = name;
+            Remark = remark;
+        }
+
+        public static IotaSeed generateIotaSeed()
+        {
+            IotaSeed iS = new IotaSeed();
+
+            iS.Seed = generateRandomSeed();
+
+            return iS;
+        }
+
+        private static string generateRandomSeed()
+        {
+            return  "seed";
+        }
+    }
+}
